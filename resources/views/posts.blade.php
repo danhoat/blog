01c323516db196d90b47214749911a4071bd23f9
unknown <danhoat@gmail.com>
2022-10-08 00:14:07 +0700

    udpate css

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,11 @@
         @foreach($posts as $post)
 
         <article class="{{ $loop->even ? 'foobar': 'noEvent'}}">
-            <h2 class="title"><a href="posts/{{$post->slug}}">{{ $post->title}}</a></h2>
+            <h2 class="title"><a href="<?php echo url('/');?>/posts/{{$post->slug}}">{{ $post->title}}</a></h2>
 
             <p> {!! $post->excerpt !!}</p>
             {{--<p>Category: {{$post->cate_name}}</>--}}
-            <p>Category:<a href="categories/{!!  $post->category->slug !!}"> {!!  $post->category->name !!} </a></p>
+            <p>Category:<a href="<?php echo url('/categories');?>/{!!  $post->category->slug !!}"> {!!  $post->category->name !!} </a></p>
 
             <p class="postinfo">
                 <span class="post-author">Author: {{ $post->author}}</span>
