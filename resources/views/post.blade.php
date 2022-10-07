82aa6c70330f6bd463cd5d243b2bee23a1593aee
unknown <danhoat@gmail.com>
2022-10-08 00:49:42 +0700

    css reset

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article>
         <h1 class="title"> <?= $post->title; ?></h1>
 
-        <p> {!! $post->content !!}</p>
+        <p class="detail"> {!! $post->content !!}</p>
         <p class="postinfo">
             <span class="post-author">Author: {{ $post->author}}</span>
             <span class="post-time">Date: {{ $post->published_at}}</span>
