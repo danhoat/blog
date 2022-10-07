b59663d9890b7801f3b66f26e5cdc294546f057d
danhoat <danhoat@gmail.com>
2022-10-07 13:44:38 +0700

    updte

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('welcome');
 });
-Route::get('posts/{post}', function ($slug) {
 
-    $path = __DIR__."/../resources/posts/{$slug}.html";
-    if( !file_exists($path) ){
-        // ddd(' file do not found.');
-        // abort(404);
-        return redirect('/');
-    }
+// Route::get('posts/{post}', function ($slug) {
 
-    $post = file_get_contents($path);
-    return view('post', [
-        'post' => $post
-    ]);
-    
-})->where('post','[A-z\-]+');
+//     $path = __DIR__."/../resources/posts/{$slug}.html";
+//     if( !file_exists($path) ){
+//         // ddd(' file do not found.');
+//         // abort(404);
+//         return redirect('/');
+//     }
+
+//     $post = file_get_contents($path);
+//     return view('post', [
+//         'post' => $post
+//     ]);
+// })->where('post','[A-z\-]+');
