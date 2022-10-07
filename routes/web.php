8364f9d844694f7535f3a9d3ce381402ce6b2a2c
unknown <danhoat@gmail.com>
2022-10-07 10:08:11 +0700

    get html  file content

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+Route::get('posts/{post}', function ($slug) {
+
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+    if( !file_exists($path) ){
+        // ddd(' file do not found.');
+        // abort(404);
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+    return view('post', [
+        'post' => $post
+    ]);
+    
+})->where('post','[A-z\-]+');
