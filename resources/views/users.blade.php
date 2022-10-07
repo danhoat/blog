28c87041645a76cecebf9adc4f8083e4413c02c4
unknown <danhoat@gmail.com>
2022-10-07 20:27:10 +0700

    table html list users

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,12 +2,21 @@
 @section('content')
     <h1> List Users</h1>
     @if($users)
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+            </tr>
 
-        @foreach($users as $user)
-            <p class="{{ $loop->even ? 'foobar': 'noEvent'}}">
-                {{$user->username}} {{$user->email}}
-            </p>
+            @foreach($users as $user)
+                    <tr class="{{ $loop->even ? 'foobar': 'noEvent'}}">
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->username}}</td>
+                        <td>{{$user->email}}</td>
+                    </tr>
         @endforeach
+        </table>
         <div class="container mt-5">
             <div class="d-flex justify-content-center pagination">
                 <!-- {!! $users->links() !!} -->
