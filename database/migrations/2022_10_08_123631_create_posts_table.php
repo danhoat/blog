38f0d9a6c3aba18df62854da8f9ce636a4f9a29a
unknown <danhoat@gmail.com>
2022-10-08 19:40:27 +0700

    udpate

---
+++ database/migrations/2022_10_08_123631_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author_id');
+            $table->foreignId('category_id');
+            $table->text('excerpt');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
