066bb46381b1f637876a76595d6c978cc0a0957d
unknown <danhoat@gmail.com>
2022-10-08 19:25:24 +0700

    fix warning

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -21,10 +21,10 @@ class PostSeeder extends Seeder
         DB::table('posts')->insert([
             'title'         => 'My First Post',
             'slug'          => 'my-first-post',
-            'author_id'       => 1,
+            'author_id'      => 1,
             'category_id'   => 1,
-            'excerpt'       => 'Excerpt of my first post',
-            'content'       => ' Full content  of my first post',
+            'excerpt'        => $this->getRandomText(120),
+            'content'       => $this->getRandomText(),
             'published_at' => $created_at
         ]);
 
@@ -33,8 +33,8 @@ class PostSeeder extends Seeder
             'slug'     => 'my-second-post',
             'author_id' => 2,
             'category_id' => 2,
-            'excerpt'  => 'Excerpt of my second post',
-            'content' => ' Full content  of my first post',
+            'excerpt' => $this->getRandomText(120),
+            'content' => $this->getRandomText(),
             'published_at' => $created_at
         ]);
         DB::table('posts')->insert([
@@ -42,8 +42,8 @@ class PostSeeder extends Seeder
             'slug'     => 'laravel-tutorial-post',
             'author_id' => 3,
             'category_id' => 2,
-            'excerpt'  => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',
-            'content' => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order. Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',
+            'excerpt' => $this->getRandomText(120),
+            'content' => $this->getRandomText(),
             'published_at' => $created_at
         ]);
         DB::table('posts')->insert([
@@ -51,8 +51,8 @@ class PostSeeder extends Seeder
             'slug'     => 'laravel-post',
             'author_id' => 3,
             'category_id' => 2,
-            'excerpt'  => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',
-            'content' => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order. Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',
+            'excerpt' => $this->getRandomText(120),
+            'content' => $this->getRandomText(),
             'published_at' => $created_at
         ]);
         DB::table('posts')->insert([
@@ -60,10 +60,26 @@ class PostSeeder extends Seeder
             'slug'     => 'laravel-post-high-level',
             'author_id' => 3,
             'category_id' => 2,
-            'excerpt'  => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',
-            'content' => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order. Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',
+            'excerpt' => $this->getRandomText(120),
+            'content' => $this->getRandomText(),
             'published_at' => $created_at
         ]);
 
+    }
+    function getRandomText($leng= 0){
+        $args = array(
+            0 => "Various versions have evolved over the years, sometimes by accident, sometimes on purpose. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            1 => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Various versions have evolved over the years, sometimes by accident, sometimes on purpose"
+            2 => "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet.\", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+            3 => "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+            4 => "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order. Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.'",
+            5 => "Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order. Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.',",
+
+        );
+        $text = $args[rand(0,3)];
+        if($leng){
+            return substr($text , 0, $leng);
+        }
+        return $text;
     }
 }
