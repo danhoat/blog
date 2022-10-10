60dc1bd5641da4ea66bd0fd378034ad977c7af1b
danhoat <danhoat@gmail.com>
2022-10-10 15:41:26 +0700

    check to show header

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
 Route::get('author/{author:username}', function (User $author) {
 
     return view('posts',[
-        'posts' => $author->posts,
+        'posts' => $author->posts()->paginate(5),
         'categories' => Category::all()
     ]);
 });
