656b1566677387230910aa3b76bc352b0e2726e9
danhoat <danhoat@gmail.com>
2022-10-12 11:56:37 +0700

    random avartar

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,10 +1,10 @@
 <article class="flex bg-gray-50 border border-gray-200 p-6 rounded-xl space-x-4" >
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" width="60" height="60" class="rounded-xl" />
+        <img src="https://i.pravatar.cc/60?img={{ $comment->author->id }}" width="60" height="60" class="rounded-xl" />
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold"> {{ $comment->author->name }}</h3>
+            <h3 class="font-bold"> {{ $comment->author->name }} {{ $comment->author->id }} </h3>
             <p class="text-xs"> Posted <time> {{ $comment->created_at->diffForHumans() }}<time></p>
         </header>
         <p> {{$comment->content }}</p>
--- resources/views/posts/detail.blade.php
+++ resources/views/posts/detail.blade.php
@@ -49,28 +49,29 @@
                 <div class="space-y-4 lg:text-lg leading-loose">{{$post->content}}</div>
             </div>
             <section  class="col-span-8 col-start-5 mt-10 space-y-6">
-                @if( auth()->user() )
+                @auth
                 <x-form_comment>
                     <form method="POST" action = "/savecomment" class="border border-gray-200  p-6 rounded-xl" id="frmComment">
                         @csrf
                         <header class="flex items-center">
 
-                            <img src="https://i.pravatar.cc/60" width="40" height="40" class="rounded-full" />
+                            <img src="https://i.pravatar.cc/60?img={{ auth()->user()->id }}" width="40" height="40" class="rounded-full" />
                             <h2 class="ml-4"> Want to participile</h2>
                         </header>
                         <div class="mt-6">
                             <input type="hidden" name="post_id" id="post_id" value="{{$post->id}}">
                             <input type="hidden" name="user_id" id="user_id" value="{{auth()->user()->id}}">
-                            <textarea name="content" id="content" class="w-full text-sm focus:outline-none focus:ring" rows="10" placeholder="Leave your message"></textarea>
-                            <div class="flex justify-end  pt-6  mt-10  border-t  border-gray-200">
+                            <textarea name="content" id="content" class="w-full text-sm focus:outline-none focus:ring" rows="6" placeholder="Leave your message"></textarea>
+                            <div class="flex justify-end  pt-6  mt-6  border-t  border-gray-200">
                                 <button class="bg-blue-500 text-white uppercase font-semibold text-xs py-3 px-10 rounded-2xl hover:bg-blue-700" type="submit" > Send</button>
                             </div>
 
                         </div>
                     </form>
                 </x-form_comment>
-                @endif
-
+                @else
+                    Please <a href="/login"> login</a> to comment on this post.
+                @endauth
                 @foreach($post->comments as $comment)
                     <x-comment :comment="$comment"></x-comment>
                 @endforeach
