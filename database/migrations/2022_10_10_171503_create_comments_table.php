5c245600d454ff56aec43a4c355f2fcd8121d7e0
xuanhoangjvit <nevergiveup.xuanhoang@gmail.com>
2023-10-06 00:23:12 +0700

    ok db

--- database/migrations/2022_10_10_171503_create_comments_table.php
+++ database/migrations/2022_10_10_171503_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        return 1;
+
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->nullable()->constrained()->cascadeOnDelete();
